406cd0e697ca4a2e5342bdfb1f078cd0d96fe1e8
Maestro1982 <kris.tote@telenet.be>
2024-03-06 14:31:59 +0100

    modify App.jsx in src folder, Hero.jsx in src components folder, index.js in src constants folder, create Benefits.jsx, CompanyLogos.jsx, Heading.jsx and Notification.jsx in src components folder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,8 @@ import { heroIcons } from '../constants';
 import Button from './Button';
 import Section from './Section';
 import Generating from './Generating';
+import Notification from './Notification';
+import CompanyLogos from './CompanyLogos';
 
 import { BackgroundCircles, BottomLine, Gradient } from './design/Hero';
 
@@ -55,7 +57,9 @@ const Hero = () => {
                   width={1024}
                   height={490}
                 />
+
                 <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[32rem] md:-translate-x-1/2' />
+
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className='hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex'>
                     {heroIcons.map((icon, index) => (
@@ -64,6 +68,13 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className='hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex'
+                    title='Code generation'
+                  />
                 </ScrollParallax>
               </div>
             </div>
@@ -81,6 +92,8 @@ const Hero = () => {
 
           <BackgroundCircles />
         </div>
+
+        <CompanyLogos className='hidden relative z-10 mt-20 lg:block' />
       </div>
       <BottomLine />
     </Section>
